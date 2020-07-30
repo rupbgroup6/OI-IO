a78a27293c4fd955ee0cf43ab54e524016fba2b7
RupBgroup6 <RupBgroup6@gmail.com>
2020-07-30 16:54:03 +0300

    Connection

--- WebApplication13/Models/Connections.cs
+++ WebApplication13/Models/Connections.cs
@@ -7,5 +7,10 @@ namespace WebApplication13.Models
 {
     public class Connections
     {
+        private string profileA;
+        private string profileB;
+
+        public string ProfileB { get => profileB; set => profileB = value; }
+        public string ProfileA { get => profileA; set => profileA = value; }
     }
 }
